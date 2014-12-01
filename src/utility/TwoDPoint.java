602e0d970259460e0e0c750c704aad4591f4829e
jason <lavalakewl@gmail.com>
2014-12-01 17:43:31 -0500

    initial version

---
+++ src/utility/TwoDPoint.java
@@ -0,0 +1,33 @@
+package utility;
+import java.io.Serializable;
+
+public class TwoDPoint implements Serializable{
+    private double x;
+    private double y;
+    public double getX() {
+        return x;
+    }
+    public void setX(double x) {
+        this.x = x;
+    }
+    public double getY() {
+        return y;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+    
+    public double distance(TwoDPoint pt){
+        double dis;
+        dis = Math.sqrt(this.x - pt.getX()) + Math.sqrt(this.y - pt.getY());
+        return dis;
+    }
+    
+    public TwoDPoint pointAdd(TwoDPoint pt){
+        TwoDPoint newP = new TwoDPoint();
+        newP.setX(this.x + pt.getX());
+        newP.setY(this.y + pt.getY());
+        return newP;
+    }
+    
+}
