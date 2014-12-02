265228fa569e2ab668ba4f2f1cbdf31b854cede3
jason <lavalakewl@gmail.com>
2014-12-01 19:14:08 -0500

    add interface datatype

---
+++ src/utility/DataTypeBase.java
@@ -0,0 +1,6 @@
+package utility;
+public interface DataTypeBase{
+    public DataTypeBase add(DataTypeBase pt);
+    public DataTypeBase distance(DataTypeBase pt);
+    public DataTypeBase div(DataTypeBase pt, double divisor);
+}
